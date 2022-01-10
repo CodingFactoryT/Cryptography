aa397fc5dc220fce6623799e698ebc7f21f55185
CodingFactoryT <94389494+CodingFactoryT@users.noreply.github.com>
2022-01-10 07:40:45 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		new One_Time_Pad_XOR(mode.decrypt, FilePaths.Test_Pic1_High_Quality);
+		new One_Time_Pad_XOR(mode.encrypt, FilePaths.Test_Pic1_High_Quality);     //select mode: encrypt or decrypt
 	}
 	
 	
