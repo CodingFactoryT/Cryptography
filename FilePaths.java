e57548362a9055cf3de9f80243fc80f7c67618f4
CodingFactoryT <94389494+CodingFactoryT@users.noreply.github.com>
2022-01-10 07:57:52 +0100

    Update FilePaths.java

--- FilePaths.java
+++ FilePaths.java
@@ -1,9 +1,9 @@
 import java.io.File;
 
 public class FilePaths {
-	public static File Test_Pic1_High_Quality = new File("\\Test_Pic1_High_Quality.jpg");
-	public static File Test_Pic2_Low_Quality = new File("\\Test_Pic2_Low_Quality.png");
-	public static File Test_Text1 = new File("\\Test_Text1.txt");
-	public static File Test_Text2 = new File("\\Test_Text2.docx");
-	public static File keyFile = new File("\\key.txt");
+	public static File Test_Pic1_High_Quality = new File("Test_Pic1_High_Quality.jpg");
+	public static File Test_Pic2_Low_Quality = new File("Test_Pic2_Low_Quality.png");
+	public static File Test_Text1 = new File("Test_Text1.txt");
+	public static File Test_Text2 = new File("Test_Text2.docx");
+	public static File keyFile = new File("key.txt");
 }
